e7b22b41b0310fc647b2c11135576fbd74f09b09
Jenifer <126200567+JeniferNatali@users.noreply.github.com>
2024-04-05 17:50:21 +0000

    TabuadaController

---
+++ app/src/main/java/application/controller/TabuadaController.java
@@ -0,0 +1,10 @@
+package application.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TabuadaController {
+
+}
+//não é porque ta escrito controller que ele é controlador, 
+//para ser um controlador tem que ter o @Controller
